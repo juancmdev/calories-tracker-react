77a70234eefcc85031fa42ee29289da171cfec5b
juancmdev <juancmorenoj@gmail.com>
2025-04-06 18:52:24 -0500

    Update Form.tsx

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,7 +2,7 @@ import { categories } from "../data/categories";
 
 export default function Form() {
   return (
-    <form className="space-y-5 bg-white shadow p-10 rounded-lg">
+    <form className="space-y-5udemy bg-white shadow p-10 rounded-lg">
       <div className="grid grid-cols-1 gap-3">
         <label htmlFor="category" className="font-bold">
           Categoría:

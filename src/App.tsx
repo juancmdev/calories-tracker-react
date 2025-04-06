b322b323eb7951a13b7c63fb894c4786cf994e81
juancmdev <juancmorenoj@gmail.com>
2025-04-06 11:00:35 -0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 function App() {
   return (
     <>
-      <h1>Contador Calorías</h1>
+      <header className="bg-lime-600">
+        <div className="max-w-4xl mx-auto flex justify-between">
+          <h1 className="text-center text-lg font-bold text-white uppercase">
+            Contador de Calorías
+          </h1>
+        </div>
+      </header>
+      <section className="bg-lime-500 py-20 px-5">
+        <div className="max-w-4xl mx-auto">
+          <p>Formulario aauí</p>
+        </div>
+      </section>
     </>
   );
 }

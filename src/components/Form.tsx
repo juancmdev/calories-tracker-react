8a58af09a49fabbcdfd925b26e46bb9ab26a178c
juancmdev <juancmorenoj@gmail.com>
2025-04-06 11:35:22 -0500

    Update Form.tsx

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -23,7 +23,23 @@ export default function Form() {
         <label htmlFor="activity" className="font-bold">
           Actividad:
         </label>
-        <input id="activity" />
+        <input
+          id="activity"
+          type="text"
+          className="border border-slate-300 p-2 rounded-lg"
+          placeholder="Ej. Comida, Jugo de Naranja, Ensalada, Ejercicio, pesas, bicicleta"
+        />
+      </div>
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="Calories" className="font-bold">
+          Calorías:
+        </label>
+        <input
+          id="Calories"
+          type="number"
+          className="border border-slate-300 p-2 rounded-lg"
+          placeholder="Calorías. Ej. 200, 300, 400"
+        />
       </div>
     </form>
   );

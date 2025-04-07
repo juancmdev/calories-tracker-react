89376b7d51af9e42afac4b13151b1b3c7432fb19
juancmdev <juancmorenoj@gmail.com>
2025-04-06 19:02:57 -0500

    Update Form.tsx

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,9 +2,11 @@ import { useState } from "react";
 import { categories } from "../data/categories";
 
 export default function Form() {
-  const [category, setCategory] = useState("");
-  const [activity, setActivity] = useState("");
-  const [calories, setCalories] = useState(0);
+  const [activity, setActivity] = useState({
+    category: "",
+    name: "",
+    calories: 0,
+  });
 
   return (
     <form className="space-y-5 bg-white shadow p-10 rounded-lg">
@@ -15,6 +17,7 @@ export default function Form() {
         <select
           className="border border-slate-300 p-2 rounded-lg w-full bg-white"
           id="category"
+          value={activity.category}
         >
           {categories.map((category) => (
             <option key={category.id} value={category.id}>
@@ -25,11 +28,11 @@ export default function Form() {
       </div>
 
       <div className="grid grid-cols-1 gap-3">
-        <label htmlFor="activity" className="font-bold">
+        <label htmlFor="name" className="font-bold">
           Actividad:
         </label>
         <input
-          id="activity"
+          id="name"
           type="text"
           className="border border-slate-300 p-2 rounded-lg"
           placeholder="Ej. Comida, Jugo de Naranja, Ensalada, Ejercicio, pesas, bicicleta"

65ac2209e81796f5bd3286dfe79a20c8b047388b
juancmdev <juancmorenoj@gmail.com>
2025-04-17 12:54:10 -0500

    Update ActivityList.tsx

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -15,7 +15,17 @@ export default function ActivityList({ activities }: ActivityListProps) {
         <div
           key={activity.id}
           className="px-5 py-10 bg-white mt-5 flex justify-between"
-        ></div>
+        >
+          <div className="space-y-2 relative">
+            <p>{activity.category}</p>
+            <p className="text-2xl font-bold pt-5">{activity.name}</p>
+            <p className="font-black text-xl text-lime-500">
+              {activity.calories} {""}
+              <span>Calorías</span>
+            </p>
+          </div>
+          <div className=""></div>
+        </div>
       ))}
     </>
   );

a2069d38082683e6273efe087d6b4dc411b713a7
juancmdev <juancmorenoj@gmail.com>
2025-04-17 12:47:28 -0500

    updated

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,9 +1,22 @@
-export default function ActivityList() {
+import { Activity } from "../types";
+
+type ActivityListProps = {
+  activities: Activity[];
+};
+
+export default function ActivityList({ activities }: ActivityListProps) {
   return (
     <>
       <h2 className="text-4xl font-bold text-slate-600 text-center">
         Comida y actividades
       </h2>
+
+      {activities.map((activity) => (
+        <div
+          key={activity.id}
+          className="px-5 py-10 bg-white mt-5 flex justify-between"
+        ></div>
+      ))}
     </>
   );
 }

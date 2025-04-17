f807f8dfd6e6d6009d37bb899f3e4cd20e09f970
juancmdev <juancmorenoj@gmail.com>
2025-04-17 11:36:54 -0500

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import { useReducer } from "react";
+
 import Form from "./components/Form";
+import { activityReducer, initialState } from "./reducers/activity-reducer";
 
 function App() {
+  const [state, dispatch] = useReducer(activityReducer, initialState);
+
   return (
     <>
       <header className="bg-lime-600">
@@ -12,7 +17,7 @@ function App() {
       </header>
       <section className="bg-lime-500 py-20 px-5">
         <div className="max-w-4xl mx-auto">
-          <Form />
+          <Form dispatch={dispatch} />
         </div>
       </section>
     </>
